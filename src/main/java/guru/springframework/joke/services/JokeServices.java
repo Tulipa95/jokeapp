00d755fe01ef3024e663e1db47900fe33206d930
Tulipa95 <maria_laura_pop@yahoo.com>
2019-12-29 22:42:27 +0200

    MVC Controller, map to root, add model an return chucknorris view

--- src/main/java/guru/springframework/joke/services/JokeServices.java
+++ src/main/java/guru/springframework/joke/services/JokeServices.java
@@ -1,5 +1,9 @@
 package guru.springframework.joke.services;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface JokeServices {
 
     String getJoke();
